abdc50c4c0e059cd1e66d5ff64012e242745a7f7
Romeo RKPK <mindaye8504@gmail.com>
2022-03-09 21:33:48 +0900

    주석 추가

--- src/main/java/org/lumenk/newdayinformation/objects/NewDayMember.java
+++ src/main/java/org/lumenk/newdayinformation/objects/NewDayMember.java
@@ -6,11 +6,25 @@ import lombok.Getter;
 
 import java.util.UUID;
 
+/**
+ * 플레이어UUID, 실명, 기수에 대한 정보를 담아둡니다
+ */
 @AllArgsConstructor
 @Builder
 @Getter
 public class NewDayMember {
+    /**
+     * 마인크래프트 플레이어 UUID입니다. 각종 연산의 기준이 됩니다
+     */
     private final UUID playerUUID;
+
+    /**
+     * 실명입니다. 개인정보 취급 동의서 받게 생겼네
+     */
     private final String name;
+
+    /**
+     * 이 분은 몇 기 입니까?
+     */
     private final Integer order;
 }
