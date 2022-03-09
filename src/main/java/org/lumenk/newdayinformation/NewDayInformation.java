824a6466e22ac2445402af842c36320b96f114b9
Romeo RKPK <mindaye8504@gmail.com>
2022-03-09 18:24:26 +0900

    리스너 등록

--- src/main/java/org/lumenk/newdayinformation/NewDayInformation.java
+++ src/main/java/org/lumenk/newdayinformation/NewDayInformation.java
@@ -1,6 +1,9 @@
 package org.lumenk.newdayinformation;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.lumenk.newdayinformation.listeners.PlayerJoinListener;
 
 public final class NewDayInformation extends JavaPlugin {
 
@@ -11,6 +14,11 @@ public final class NewDayInformation extends JavaPlugin {
         instance = this;
         getLogger().info("몰?루");
         getLogger().info("아!루");
+
+        final PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(new PlayerJoinListener(), this);
+
+
 
     }
 
