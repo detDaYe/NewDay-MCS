627d6ad0edfe6d1acad282e02a086f70f45d4e2f
Romeo RKPK <mindaye8504@gmail.com>
2022-03-15 18:53:23 +0900

    DataCommand 추가

--- src/main/java/org/lumenk/newdayinformation/commands/NewDayInfoCommand.java
+++ src/main/java/org/lumenk/newdayinformation/commands/NewDayInfoCommand.java
@@ -4,10 +4,25 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
+import org.lumenk.newdayinformation.repositories.MemberRepository;
 
 public class NewDayInfoCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        return false;
+        if(args.length == 0)
+            return false;
+
+        switch (args[0]){
+            case "load" ->{
+                MemberRepository.load();
+            }
+
+            case "save"->{
+                MemberRepository.save();
+            }
+
+            default -> {return  false;}
+        }
+        return true;
     }
 }
