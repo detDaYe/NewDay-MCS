98c97ebd12afe1833fce9eaf5022e4583c04f28e
Romeo RKPK <mindaye8504@gmail.com>
2022-03-15 18:34:11 +0900

    work in progress

---
+++ src/main/java/org/lumenk/newdayinformation/commands/NewDayInfoCommand.java
@@ -0,0 +1,13 @@
+package org.lumenk.newdayinformation.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class NewDayInfoCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return false;
+    }
+}
