492a346877321b31f73682f85b5b951d1ca8f4b0
Romeo RKPK <mindaye8504@gmail.com>
2022-03-17 22:21:13 +0900

    렌더러 교체 완료

--- src/main/java/org/lumenk/newdayinformation/listeners/ChatListener.java
+++ src/main/java/org/lumenk/newdayinformation/listeners/ChatListener.java
@@ -6,15 +6,20 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.lumenk.newdayinformation.chatrenders.NewDayChatRender;
+import org.lumenk.newdayinformation.chatrenders.YBRenderer;
+import org.lumenk.newdayinformation.objects.NewDayMember;
+import org.lumenk.newdayinformation.repositories.MemberRepository;
 
 public class ChatListener implements Listener {
-    final ChatRenderer renderer = new NewDayChatRender();
-
+    final YBRenderer ybRenderer = new YBRenderer();
     @EventHandler
     public void onChat(AsyncChatEvent event){
         //Compoevent.originalMessage();
         final Player player = event.getPlayer();
-        event.renderer(renderer);
+        final NewDayMember member = MemberRepository.getMember(player.getUniqueId());
+        if(member == null)return;
+        if(member.getOrder() == -1)return;
+        event.renderer(ybRenderer);
     }
 
 }
